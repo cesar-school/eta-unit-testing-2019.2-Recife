4fc8ff783d3e992da3c12e3952672c0f13d49b7a
Milton Castro <milton.castro@inovant.com.br>
2020-08-01 10:18:07 -0300

    Add Body Mass Index calculate and classify

--- src/test/java/school/cesar/eta/unit/BodyMassIndexTest.java
+++ src/test/java/school/cesar/eta/unit/BodyMassIndexTest.java
@@ -6,31 +6,31 @@ import org.junit.jupiter.api.Test;
 public class BodyMassIndexTest {
 
     @Test
-    public void calculate_bmiUnder16_severelyUnderweight() {
-        String actual = BodyMassIndex.calculate(15.9f);
+    public void classify_bmiUnder16_severelyUnderweight() {
+        String actual = BodyMassIndex.classify(15.9f);
         Assertions.assertEquals("Severely Underweight", actual);
     }
 
     @Test
-    public void calculate_bmi16_underweight() {
-        String actual = BodyMassIndex.calculate(16f);
+    public void classify_bmi16_underweight() {
+        String actual = BodyMassIndex.classify(16f);
         Assertions.assertEquals("Underweight", actual);
     }
 
     @Test
-    public void calculate_18Dot5_healthyWeight() {
-        String actual = BodyMassIndex.calculate(18.5f);
+    public void classify_18Dot5_healthyWeight() {
+        String actual = BodyMassIndex.classify(18.5f);
         Assertions.assertEquals("Healthy Weight", actual);
     }
 
     @Test
-    public void calculate_bmi25_overweight() {
-        Assertions.assertEquals("Overweight", BodyMassIndex.calculate(25));
+    public void classify_bmi25_overweight() {
+        Assertions.assertEquals("Overweight", BodyMassIndex.classify(25));
     }
 
     @Test
-    public void calculate_bmi30_obese() {
-        Assertions.assertEquals("Obese", BodyMassIndex.calculate(30));
+    public void classify_bmi30_obese() {
+        Assertions.assertEquals("Obese", BodyMassIndex.classify(30));
     }
 
 }

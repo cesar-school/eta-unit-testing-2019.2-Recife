d3910018584a41f71a9caeaef3a70558525082bc
Milton Castro <milton.castro@inovant.com.br>
2020-08-01 09:55:40 -0300

    My application test double

---
+++ src/main/java/school/cesar/eta/unit/Message.java
@@ -0,0 +1,18 @@
+package school.cesar.eta.unit;
+
+public class Message {
+    private String msg = null;
+    private String rec = null;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getRec() {
+        return rec;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
